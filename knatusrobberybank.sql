bade5f387ef80d7aab461cf338d83e5df442c4cf
Knaak53 <manugara53@gmail.com>
2019-06-01 06:35:24 +0200

    add sql file

---
+++ knatusrobberybank.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('blowtorch', 'Soplete', 1, 0, 1),
+('c4_bank', 'C4 de 10 kilos', 1, 0, 1),
+('rasperry', 'Rasperry', 1, 0, 1);
